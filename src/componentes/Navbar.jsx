aaed302a99f15e16346cdb1681ed09ba5d39f7d9
Agustín Biancardi <agustinbiancardi02@gmail.com>
2022-02-21 21:59:58 -0300

    Agregar barra de navegación

---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+    return (
+        <div className="menu">
+            <a className="Enlinea" href="#">D´gustar</a>
+            <nav className="Enlinea">
+                <ul>
+                    <li className="Enlinea"><a href="#">Inicio</a></li>
+                    <li className="Enlinea"><a href="#">Productos</a></li>
+                    <li className="Enlinea"><a href="#">FAQS</a></li>
+                    <li className="Enlinea"><a href="#">Nosotros</a></li>
+                    <li className="Enlinea"><a href="#">Blog</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
