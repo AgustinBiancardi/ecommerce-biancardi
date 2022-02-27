29d7f913b9430df9b56384f294d1d88bc04e56cc
Agustín Biancardi <agustinbiancardi02@gmail.com>
2022-02-27 15:53:04 -0300

    Agregando Bootstrap al proyecto

--- src/componentes/cartWidget/CartWidget.jsx
+++ src/componentes/cartWidget/CartWidget.jsx
@@ -1,7 +1,9 @@
+import carrito from "../../images/carrito.png"
+
 function CartWidget (){
 return(
     <div>
-        <img src="https://png.pngtree.com/element_our/20190528/ourlarge/pngtree-flat-shopping-cart-image_1127469.jpg" width={50}  alt="" />
+        <img src={carrito} width={50}  alt="" />
     </div>
 )
 }
--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -1,25 +1,55 @@
-import './navbar.css';
 import CartWidget from '../cartWidget/CartWidget';
-import logo from "../../images/logoo.png"
+import logo from "../../images/logo.png";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 
-function Navbar() {
-  
-    return (
-        <div className="menu">
-            <a className="Enlinea" href="/"><img src={logo} alt="" /></a>
-            <nav className="Enlinea">
-                <ul>
-                    <li className="Enlinea"><a href="/">Inicio</a></li>
-                    <li className="Enlinea"><a href="/">Productos</a></li>
-                    <li className="Enlinea"><a href="/">FAQS</a></li>
-                    <li className="Enlinea"><a href="/">Nosotros</a></li>
-                    <li className="Enlinea"><a href="/"><CartWidget/></a></li>
-                     
-                    
-                </ul>
-            </nav>
-        </div>
-    )
+function NavBar() {
+  return (
+      <div>
+
+    <Navbar bg="light" expand="lg">
+        <Container fluid>
+        <Navbar.Brand href="/">
+            <img
+                src={logo}
+                width="70"
+                height="70"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+            />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+            >
+                <Nav.Link href="/">Tienda</Nav.Link>       
+                <Nav.Link href="/">Recetas</Nav.Link>
+                <Nav.Link href="/">Blog</Nav.Link> 
+                <Nav.Link href="/">Nosotros</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+                <FormControl
+                type="search"
+                placeholder="Buscar producto"
+                className="me-2"
+                aria-label="Search"
+                />
+                <Button variant="outline-success">Buscar</Button>
+            </Form>
+            <Nav.Link href="/"><CartWidget/></Nav.Link>
+        </Navbar.Collapse>
+    </Container>
+    </Navbar>
+    </div>
+    
+  )
 }
 
-export default Navbar
+export default NavBar
